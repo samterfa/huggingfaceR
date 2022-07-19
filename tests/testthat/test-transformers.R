641758375a159027010c2766e90cca7f9085b0b7
Sam Terfa <samterfa@gmail.com>
2022-07-18 19:41:35 -0500

    test

--- tests/testthat/test-transformers.R
+++
@@ -1,9 +0,0 @@
-test_that("load model fails if not given correct model nae", {
-  expect_error(hf_load_model(model_id = "x not a model x"))
-})
-
-
-tokenizer <- hf_load_tokenizer("distilbert-base-uncased")
-test_that("load tokenizer function loads correctly", {
-  expect_true("vocab" %in% names(tokenizer))
-})
